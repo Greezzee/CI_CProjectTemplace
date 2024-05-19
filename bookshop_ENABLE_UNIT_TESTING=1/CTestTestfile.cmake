2594987ee70590c3f33c2ec798d0a08c55209eb1
Zorin Eugene <39644926+Greezzee@users.noreply.github.com>
2024-05-19 15:46:55 +0300

    renaming done

--- bookshop_ENABLE_UNIT_TESTING=1/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/Study/Verification/verif_test2
-# Build directory: /mnt/d/Study/Verification/verif_test2/bookshop_ENABLE_UNIT_TESTING=1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
--- bookshop_ENABLE_UNIT_TESTING=1/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/Study/Verification/verif_test2/test
-# Build directory: /mnt/d/Study/Verification/verif_test2/bookshop_ENABLE_UNIT_TESTING=1/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tmp_test "/mnt/d/Study/Verification/verif_test2/bookshop_ENABLE_UNIT_TESTING=1/test/tmp_test_Tests")
-set_tests_properties(tmp_test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/d/Study/Verification/verif_test2/test/CMakeLists.txt;76;add_test;/mnt/d/Study/Verification/verif_test2/test/CMakeLists.txt;0;")
